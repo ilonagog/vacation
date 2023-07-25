ab5e1274e0e02d8744444d20e2101388e44d914a
ilona <gogiashvili.ilona@gmail.com>
2023-07-25 10:00:47 -0400

    Created Home.js and NavBar.js installed react router6, created test links in the NavBar

---
+++ client/src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1>"Hello"</h1>
+            <Link>Home</Link>
+            <Link>Booking</Link>
+            <Link>Reviews</Link>
+        </div>
+    )
+}
+
+export default NavBar
