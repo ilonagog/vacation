8080537fd0b8e2ecb14291a6f3b9920f459cffc1
ilona <gogiashvili.ilona@gmail.com>
2023-07-27 20:31:08 -0400

    created addPlace function

--- client/src/PlaceForm.js
+++ client/src/PlaceForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-const PlaceForm = () => {
-    const [place, setPlace] = useState({
+const PlaceForm = ({ addPlace }) => {
+    const [input, setInput] = useState({
         name: "",
         location: "",
         image: "",
@@ -10,26 +10,32 @@ const PlaceForm = () => {
 
     })
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+
 
     }
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addPlace(input)
     }
     return (
         <div>
             <form className="form" onSubmit={handleSubmit}>
                 <h3>You can add a new Location</h3>
                 <label htmlFor='name'>Name :</label>
-                <input type="text" name="name" value={place.name} onChange={handleChange} />
+                <input type="text" name="name" value={input.name} onChange={handleChange} />
                 <label htmlFor='location'>Location :</label>
-                <input type="text" name="location" value={place.location} onChange={handleChange} />
+                <input type="text" name="location" value={input.location} onChange={handleChange} />
                 <label htmlFor='image'>Image :</label>
-                <input type="text" name="image" value={place.image} onChange={handleChange} />
+                <input type="text" name="image" value={input.image} onChange={handleChange} />
                 <label htmlFor='description'>Description</label>
-                <input type="text" name="description" value={place.description} onChange={handleChange} />
+                <input type="text" name="description" value={input.description} onChange={handleChange} />
                 <label htmlFor='price'>Price :</label>
-                <input type="text" name="price" value={place.price} onChange={handleChange} />
+                <input type="text" name="price" value={input.price} onChange={handleChange} />
                 <input type="submit" />
             </form>
         </div>
--- client/src/Places.js
+++ client/src/Places.js
@@ -3,7 +3,7 @@ import PlaceCard from './PlaceCard'
 import PlaceForm from './PlaceForm'
 
 
-const Packages = ({ places }) => {
+const Packages = ({ places, setPlaces }) => {
     const [viewAddForm, setViewAddForm] = useState(false)
 
     const placesList = places.map((place) => (<PlaceCard key={place.id} place={place} />))
@@ -12,13 +12,34 @@ const Packages = ({ places }) => {
         setViewAddForm(true)
     }
 
+    const addPlace = (place) => {
+        fetch("/places", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                name: place.name,
+                location: place.location,
+                image: place.image,
+                description: place.description,
+                price: place.price
+            })
+        })
+            .then((resp) => resp.json())
+            .then(data => {
+                setPlaces([...places, data]);
+            })
+        setViewAddForm(false)
+    }
+
 
     return (
         <div>
             <button className='button' onClick={handleClick}>Add New Property</button>
             <br />
             {viewAddForm ?
-                <PlaceForm />
+                <PlaceForm addPlace={addPlace} />
                 :
                 <ul> {placesList}</ul>
             }
